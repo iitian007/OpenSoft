5909d2bcd8f47b952786025d3f22b92a18495138
iitian007 <33984763+iitian007@users.noreply.github.com>
2018-11-04 12:28:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,133 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+#from data import Articles
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+import pymysql
+import pymysql.cursors
+app = Flask(__name__)
+
+# Config pyMySQL
+db = pymysql.connect(host='localhost',
+                    user='root',
+                    password='',
+                    db='newtry',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+
+# Index
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# About
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/dashboard')
+def about1():
+    return render_template('dashboard.html')
+
+# Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(form.password.data)
+
+        # Create cursor
+        cursor=db.cursor()
+
+        # Execute query
+        cursor.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        cursor.connection.commit()
+
+        # Close connection
+        cursor.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cursor = db.cursor()
+
+        # Get user by username
+        result = cursor.execute("SELECT * FROM users WHERE username = %s", username)
+
+        if result >0:
+            # Get stored hash
+            data = cursor.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('about'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
